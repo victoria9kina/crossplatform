bec6e36b092418b6962eb6ad8142049e89b916b3
victoria9kina <victoria9kina@gmail.com>
2024-10-08 17:09:56 +0300

    Итог
    изменена функция view_all, save, edit_pipe, edit_CS

--- lab1_vdevyatkina/lab1_vdevyatkina.cpp
+++ lab1_vdevyatkina/lab1_vdevyatkina.cpp
@@ -4,24 +4,22 @@
 
 using namespace std;
 
-//структуры
 struct Pipe
 {
-    string Name; //название трубы
-    float length; //километры 
-    int diameter; //миллиметры
-    bool repair; //признак "в ремонте" 
+    string Name; 
+    float length; 
+    int diameter; 
+    bool repair; 
 };
 
 struct Station
 {
-    string Name; //название КС
-    int workshops; //Кол-во цехов
-    int workshopsinwork; //Кол-во цехов в работе
-    int effectiveness; //Коэфф. эффективности
+    string Name; 
+    int workshops;
+    int workshopsinwork; 
+    int effectivity; 
 };
 
-//Функции
 int check_int(int max, int low) {
     int z;
     cin >> z;
@@ -91,7 +89,7 @@ int menu() {
 }
 
 void new_pipe(Pipe& P) {
-    cout << "Enter the pipe name (in English): ";
+    cout << "Enter the pipe name: ";
     getline(cin, P.Name);
     cout << "Enter the pipe length in kilometers: ";
     P.length = floatcheck(0, 0.1);
@@ -99,7 +97,6 @@ void new_pipe(Pipe& P) {
     P.diameter = check_int(0, 1);
     cout << "Select the pipe's status: " << endl << "0) Not under repair" << endl << "1) Under repair" << endl;
     P.repair = check_int(1, 0);
-    cout << "Pipe created: " << endl << "Pipe name: " << P.Name << "; Pipe length: " << P.length << "; Pipe diameter: " << P.diameter << "; Repair status: " << boolalpha << P.repair << endl;
 }
 
 void new_ks(Station& CS) {
@@ -110,50 +107,46 @@ void new_ks(Station& CS) {
     cout << "Enter the number of workshops in operation: ";
     CS.workshopsinwork = check_int(CS.workshops, 0);
     cout << "Enter the CS efficiency coefficient (from 0 to 100): ";
-    CS.effectiveness = check_int(100, 0);
-    cout << "Compressor station created: " << endl << "CS name: " << CS.Name << "; Workshops: "  << CS.workshops << "; Workshops in operation: " << CS.workshopsinwork << "; CS efficiency coefficient: " << CS.effectiveness << endl;
+    CS.effectivity = check_int(100, 0);
 }
 
 void show_Pipe(const Pipe& P) {
     cout << "Pipe: " << endl << "Pipe name: " << P.Name << "; Pipe length: " << P.length << "; Pipe diameter: " << P.diameter << "; Repair status: " << boolalpha << P.repair << endl;
 }
 void show_cs(const Station& CS) {
-    cout << "Compressor station: " << endl << "CS name: " << CS.Name << "; Workshops: " << CS.workshops << "; Workshops in operation: " << CS.workshopsinwork << "; Efficiency coefficient: " << CS.effectiveness << endl;
+    cout << "Compressor station: " << endl << "CS name: " << CS.Name << "; Workshops: " << CS.workshops << "; Workshops in operation: " << CS.workshopsinwork << "; Efficiency coefficient: " << CS.effectivity << endl;
 }
 
 void view_all(const Pipe& P, const Station& CS) {
-    if (CS.workshops > 0 && P.diameter > 0) {
+    bool pipe_exists = P.diameter > 0;
+    bool cs_exists = CS.workshops > 0;
+
+    if (pipe_exists) {
         show_Pipe(P);
-        show_cs(CS);
-    }
-    else if (CS.workshops < 1 && P.diameter > 0) {
-        show_Pipe(P);
-        cout << "CS has not been added yet!" << endl;
-    }
-    else if (CS.workshops > 0 && P.diameter < 1) {
-        show_cs(CS);
-        cout << "Pipe has not been added yet!" << endl;
     }
     else {
-        cout << "You haven't added any objects yet." << endl;
+        cout << "Pipe has not been added yet!" << endl;
+    }
+    if (cs_exists) {
+        show_cs(CS);
+    }
+    else {
+        cout << "CS has not been added yet!" << endl;
     }
 }
 
+
 void edit_Pipe(Pipe& P) {
     int m = 0;
-    if (P.diameter > 0) {
-        while (true) {
-            cout << "Select the parameter you want to edit: " << endl << "1) 'Under repair' status" << endl << "0) Exit to menu" << endl << "Command number: ";
-            m = check_int(1, 0);
-            if (m == 0) {
-                break;
-            }
-            else if (m == 1) {
-                cout << "Old pipe status: under repair " << boolalpha << P.repair << endl;
-                cout << "Select the new pipe status: " << endl << "0) Not under repair" << endl << "1) Under repair" << endl << "Enter the command number: ";
-                P.repair = check_int(1, 0);
-            }
-        }
+    if (P.diameter > 0) 
+    {
+        cout << "Select the parameter you want to edit: " << endl << "1) 'Under repair' status" << endl << "0) Exit to menu" << endl << "Command number: ";
+        m = check_int(1, 0);
+        if (m == 0)
+            return;
+        cout << "Old pipe status: under repair " << boolalpha << P.repair << endl;
+        cout << "Select the new pipe status: " << endl << "0) Not under repair" << endl << "1) Under repair" << endl << "Enter the command number: ";
+        P.repair = check_int(1, 0);
     }
     else {
         cout << "You haven't added a pipe yet and cannot view its parameters. Please configure the pipe in the menu (option 1)" << endl;
@@ -161,25 +154,22 @@ void edit_Pipe(Pipe& P) {
 }
 void edit_CS(Station& CS) {
     int m = 0;
-    if (CS.workshops > 0) {
-        while (true) {
-            cout << "Select the parameter you want to edit: " << endl << "1) Number of workshops in operation" << endl << "0) Exit to menu" << endl << "Command number: ";
-            m = check_int(1, 0);
-            if (m == 0) {
-                break;
-            }
-            else if (m == 1) {
-                cout << "Total number of workshops: " << CS.workshops << "; Old number of workshops in operation: " << CS.workshopsinwork << endl;
-                cout << "Enter the new number of workshops in operation: ";
-                CS.workshopsinwork = check_int(CS.workshops, 0);
-            }
-        }
+    if (CS.workshops > 0) 
+    {       
+        cout << "Select the parameter you want to edit: " << endl << "1) Number of workshops in operation" << endl << "0) Exit to menu" << endl << "Command number: ";
+        m = check_int(1, 0);
+        if (m == 0) 
+            return;                  
+        cout << "Total number of workshops: " << CS.workshops << "; Old number of workshops in operation: " << CS.workshopsinwork << endl;
+        cout << "Enter the new number of workshops in operation: ";
+        CS.workshopsinwork = check_int(CS.workshops, 0);     
     }
     else {
         cout << "You haven't added a CS yet and cannot view its parameters. Please configure the CS in the menu (option 2)" << endl;
     }
-}void SavePipe(const Pipe& P, ofstream& out)
-{
+}
+void SavePipe(const Pipe& P, ofstream& out){
+
     out << "data Pipe:" << endl;
     out << P.Name << endl;
     out << P.length << " " << P.diameter << " " << P.repair << endl;
@@ -189,7 +179,7 @@ void SaveCS(const Station& CS, ofstream& out)
 {
     out << "data CS:" << endl;
     out << CS.Name << endl;
-    out << CS.workshops << " " << CS.workshopsinwork << " " << CS.effectiveness << endl;;
+    out << CS.workshops << " " << CS.workshopsinwork << " " << CS.effectivity << endl;;
 }
 
 void loadPipe(Pipe& P, ifstream& in)
@@ -202,27 +192,24 @@ void loadPipe(Pipe& P, ifstream& in)
 void loadCS(Station& CS, ifstream& in) {
     if (in.is_open()) {
         getline(in >> ws, CS.Name);
-        in >> CS.workshops >> CS.workshopsinwork >> CS.effectiveness;
+        in >> CS.workshops >> CS.workshopsinwork >> CS.effectivity;
     }
 }
 void save(const Pipe& P, const Station& CS) {
     ofstream out;
     out.open("info.txt");
-    if (out.is_open())
-    {
+    if (out.is_open()) {
         if (P.Name.empty() && CS.Name.empty()) {
             cout << "You have no data to save!" << endl;
             return;
         }
-        if (!P.Name.empty() && CS.Name.empty()) {
+        if (!P.Name.empty()) {
             SavePipe(P, out);
-            cout << "Pipe data saved!" << endl;
         }
-        if (!CS.Name.empty() && P.Name.empty()) {
+        if (!CS.Name.empty()) {
             SaveCS(CS, out);
-            cout << "CS data saved!" << endl;
         }
-
+        cout << "data saved!" << endl; 
     }
     out.close();
 }
@@ -271,7 +258,7 @@ int main() {
             edit_CS(CS);
             break;
         case 6:
-            save(P, CS);
+            save(P, CS);//!!!!!!!!!!
             break;
         case 7:
             load(P, CS);
